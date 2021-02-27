fdc2bde8289c5976b2ba6c6488a7bde805bd875d
Dakota Brown <dakota.kae.brown@gmail.com>
2021-02-27 09:16:31 -0700

    Updating author information and code URL

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="pygiftbit",
-    version="1.0.1",
+    version="1.0.2",
     description="A simple Python wrapper for the Giftbit API",
     long_description=README,
     long_description_content_type="text/markdown",
-    author="Donald Brown",
-    author_email="donald.k.brown3@gmail.com",
-    url="https://github.com/donaldkbrown/pygiftbit",
+    author="Dakota Brown",
+    author_email="dakota.kae.brown@gmail.com",
+    url="https://github.com/da-code-a/pygiftbit",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
